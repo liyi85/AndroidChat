f63bfbbf3f102278ec29cc3b960cc976d93927e3
liyi85 <liyi85@gmail.com>
2016-06-12 10:54:51 -0500

    implementamos la libreria greenrobotevenbus para regiter, unregister y post

--- app/src/main/java/com/example/andrearodriguez/androidchat/lib/GreenRobotEventBus.java
+++ app/src/main/java/com/example/andrearodriguez/androidchat/lib/GreenRobotEventBus.java
@@ -1,21 +1,41 @@
 package com.example.andrearodriguez.androidchat.lib;
 
+import org.greenrobot.eventbus.EventBus;
+
 /**
  * Created by andrearodriguez on 6/12/16.
  */
 public class GreenRobotEventBus implements EvenBus {
+
+
+    EventBus eventBus;
+    private static class SingletonHolder{
+        private static final GreenRobotEventBus INSTANCE = new GreenRobotEventBus();
+    }
+
+    public static GreenRobotEventBus getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+
+    public GreenRobotEventBus(){
+        this.eventBus = EventBus.getDefault();
+    }
+
     @Override
     public void register(Object subscriber) {
+        eventBus.register(subscriber);
 
     }
 
     @Override
     public void unregister(Object subscriber) {
+        eventBus.unregister(subscriber);
 
     }
 
     @Override
     public void post(Object event) {
+        eventBus.post(event);
 
     }
 }
